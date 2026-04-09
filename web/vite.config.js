ca0326ddaf8eb429054fab2db07e2eab9a0520ac
wanghuai <305420651@qq.com>
2026-04-09 14:56:06 +0800

    --

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pokedex/',
+  base: "/pokedex/",
   plugins: [vue()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: 6005,
+  },
+});
